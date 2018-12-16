d29d00d52f2860695999ca9a2e7a831007e87843
Thiago Colares <thicolares@gmail.com>
2018-12-16 13:46:51 -0300

    Create a mock comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src="https://via.placeholder.com/200x200" alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        John Doe
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 ReactDOM.render(
