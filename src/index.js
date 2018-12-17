2b87e0f9d334a0f17b612794c6df22b2a5ab5f13
Thiago Colares <thicolares@gmail.com>
2018-12-16 21:17:16 -0300

    Triplicate component

--- src/index.js
+++ src/index.js
@@ -11,7 +11,35 @@ const App = () => {
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        John Doe
+                        {`${faker.name.firstName()} ${faker.name.lastName()}`}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={ faker.image.avatar() } alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {`${faker.name.firstName()} ${faker.name.lastName()}`}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={ faker.image.avatar() } alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {`${faker.name.firstName()} ${faker.name.lastName()}`}
                     </a>
                     <div className="metadata">
                         <span className="date">Today at 6:00PM</span>
