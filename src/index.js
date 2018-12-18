09c132ac48091f06deca676a33d007f89bee4640
Thiago Colares <thicolares@gmail.com>
2018-12-17 22:45:55 -0300

    Pass plain text as props children into ApprovalCard

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ const App = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail
                     author={faker.name.firstName()}
                     avatar={faker.image.avatar()}
